a029589e543f4ef98a140ec4b21d75fd2acf6327
brian-tura <b.tura2002@gmail.com>
2025-05-13 15:20:18 +0200

    [DONE] Excercise done plus bonus

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,53 @@ import React from 'react'
 
 function App() {
   const [actors, setActors] = useState([])
+  const [actresses, setActresses] = useState([])
 
   const fetchActors = () => {
     axios
+      .get("https://lanciweb.github.io/demo/api/actors/")
+      .then((res) => {
+        setActors(res.data)
+        console.log(res.data)
+      })
+  };
+
+  const fetchActresses = () => {
+    axios
     .get("https://lanciweb.github.io/demo/api/actresses/")
     .then((res) => {
-      setActors(res.data)
+      setActresses(res.data)
       console.log(res.data)
     })
-  };
-  
+  }
+
   useEffect(() => {
     fetchActors()
+    fetchActresses()
   }, [])
+
+  const allActors = [...actors, ...actresses]
+  console.log(allActors)
 
   return (
     <>
-      
+      <main>
+        <div className="container">
+          <div className="row">
+            {actors.map((actor) => {
+              return (
+                <div key={actor.id} className='card col-4'>
+                  <img src={actor.image} alt="" />
+                  <h3>{actor.name}</h3>
+                  <p>{actor.birth_year} {actor.nationality}</p>
+                  <p>{actor.biography}</p>
+                  <p>{actor.awards}</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </main>
     </>
   )
 }
